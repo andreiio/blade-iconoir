c9aa1fff6041f2e8a32e36f9105348ea8b984d96
Andrei Ioniță <hi@andrei.io>
2024-02-26 08:15:18 +0000

    feat: use original naming convention (#64)
    * feat: use original naming convention
    
    BREAKING CHANGE: changes the file structure
    
    * trigger workflow
    
    * fix tar
    
    * chore: update to iconoir v7.4.0 (#63)
    
    * test: fix tests
    
    * docs: update readme
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Arne Becker <buddhaCode@users.noreply.github.com>

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -9,7 +9,7 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_compiles_a_single_anonymous_component()
     {
-        $result = svg('iconoir-regular-activity')->toHtml();
+        $result = svg('iconoir-activity')->toHtml();
 
         $expected = <<<'SVG'
         <svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -23,7 +23,7 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_classes_to_icons()
     {
-        $result = svg('iconoir-regular-activity', 'w-6 h-6 text-gray-500')->toHtml();
+        $result = svg('iconoir-activity', 'w-6 h-6 text-gray-500')->toHtml();
 
         $expected = <<<'SVG'
         <svg class="w-6 h-6 text-gray-500" width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -37,7 +37,7 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_styles_to_icons()
     {
-        $result = svg('iconoir-regular-activity', ['style' => 'fill: #F00'])->toHtml();
+        $result = svg('iconoir-activity', ['style' => 'fill: #F00'])->toHtml();
 
         $expected = <<<'SVG'
         <svg style="fill: #F00" width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
